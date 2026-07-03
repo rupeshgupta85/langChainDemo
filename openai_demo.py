3891196048091056bea9153cb494a799e99d767f
Rupesh Gupta <rupeshgupta85@users.noreply.github.com>
2026-07-03 17:02:31 -0400

    Initial commit for langChainDemo

---
+++ openai_demo.py
@@ -0,0 +1,22 @@
+
+import os
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+# Set your Bearer token directly (or use .env)
+#API_TOKEN = "O70Q3NzyiohFbCzmxNqS9Oj6Td90pWB5M74cGQp3"
+
+load_dotenv()  # Load environment variables from .env file
+
+API_TOKEN = os.getenv("OPENAI_API_KEY")
+
+llm = ChatOpenAI(
+    model="amazon.nova-pro-v1:0",  # or whatever model your endpoint supports
+    api_key=API_TOKEN,
+    base_url="https://openai.generative.engine.capgemini.com/v1"  # ✅ important
+)
+
+question = input("Enter the question you want to ask: ")
+response = llm.invoke(question)
+
+print(response.content)
---
+++ streamlit_demo.py
@@ -0,0 +1,28 @@
+
+import os
+from langchain_openai import ChatOpenAI
+import streamlit as st
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
+from dotenv import load_dotenv
+
+# Set your Bearer token directly (or use .env)
+#API_TOKEN = "O70Q3NzyiohFbCzmxNqS9Oj6Td90pWB5M74cGQp3"
+
+load_dotenv()  # Load environment variables from .env file
+
+API_TOKEN = os.getenv("OPENAI_API_KEY")
+
+llm = ChatOpenAI(
+    model="amazon.nova-pro-v1:0",  # or whatever model your endpoint supports
+    api_key=API_TOKEN,
+    base_url="https://openai.generative.engine.capgemini.com/v1"  # ✅ important
+)
+
+#question = input("Enter the question you want to ask: ")
+st.title("Ask anything.....")
+question = st.text_input("Enter the question you want to ask:")
+
+
+if question:
+    response = response = llm.invoke(question)
+    st.write(response.content)
